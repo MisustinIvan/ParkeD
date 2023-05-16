010fcd76e58e7786eb933aef433c771e80592680
“Mauterix” <martin.safrata@gmail.com>
2023-05-16 14:22:16 +0200

    trying to add sensors for like 4 hours

--- src/hardware/esp32/ESP32 - Parking system/src/main.cpp
+++ src/hardware/esp32/ESP32 - Parking system/src/main.cpp
@@ -1,9 +1,39 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "pins.h"
+#include "libs.h"
 
-void setup() {
-  // put your setup code here, to run once:
+#pragma region definitions
+Ultrasonic ultrasonic1(25, 33);
+Ultrasonic ultrasonic2(32, 35);
+Ultrasonic ultrasonic3(18, 19);
+
+#pragma endregion
+
+#pragma region functions
+void sendData(int id, bool value){
+  Serial.println("BtnPressed");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+#pragma endregion
+
+
+void setup(){
+  pinMode(btnPin,INPUT_PULLUP);
+  Serial.begin(115200);
+  
+  
+}
+
+void loop(){
+  long microsec = ultrasonic1.timing();
+  long microsec = ultrasonic2.timing();
+  long microsec = ultrasonic3.timing();
+  float dist1 = float(ultrasonic1.read());
+  float dist2 = float(ultrasonic2.read());
+  float dist3 = float(ultrasonic3.read());
+  //Serial.println("Dist1: " + String(dist1));
+  Serial.println("Dist2: " + String(dist2));
+  Serial.println("Dist3: " + String(dist3));
+  delay(1000);
 }
